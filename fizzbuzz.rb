8474e5e55204282d8e2c144d2585dda93ad4d418
Alan Chodyniecki <alan.chodyniecki@softwire.com>
2022-09-08 12:36:37 +0100

    Added FizzBuzz functionality

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,21 @@
 def fizzbuz
-  puts 'Hello world!'
+  (1..100).each { |i|
+    fizz_buzz_str = ''
+    fizz_or_buzz = false
+    if i % 3 == 0
+      fizz_buzz_str += 'Fizz'
+      fizz_or_buzz = true
+    end
+    if i % 5 == 0
+      fizz_buzz_str += 'Buzz'
+      fizz_or_buzz = true
+    end
+    if fizz_or_buzz
+      puts fizz_buzz_str
+    else
+      puts i
+    end
+  }
 end
 
 fizzbuz
