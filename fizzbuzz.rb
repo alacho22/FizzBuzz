0c5a0ced831557a8e809bc39aac51e3579c70fc0
Alan Chodyniecki <alan.chodyniecki@softwire.com>
2022-09-09 10:20:15 +0100

    added custom rules

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,20 @@
+def is_rule(rules, rule_no)
+  rules.include?(rule_no)
+end
+
 def fizzbuz(max, rules)
   (1..max).each { |i|
 
     result = []
     print_number = true
-    is_bong = i % 11 == 0
+    is_bong = is_rule(rules, 11) && i % 11 == 0
 
-    if (not is_bong) && i % 3 == 0
+    if (not is_bong) && is_rule(rules, 3) && i % 3 == 0
         result << 'Fizz'
         print_number = false
     end
 
-    if i % 13 == 0
+    if is_rule(rules, 13) && i % 13 == 0
       result << 'Fezz'
       print_number = false
     end
@@ -18,16 +22,16 @@ def fizzbuz(max, rules)
       result << 'Bong'
       print_number = false
     else
-      if i % 5 == 0
+      if is_rule(rules, 5) && i % 5 == 0
         result << 'Buzz'
         print_number = false
       end
-      if i % 7 == 0
+      if is_rule(rules, 7) && i % 7 == 0
         result << 'Bang'
         print_number = false
       end
     end
-    if i % 17 == 0
+    if is_rule(rules, 17) && i % 17 == 0
       result.reverse!
     end
     if print_number
